f7b659972d473b3ea854ebeb78520c2bb24f9af4
Apryn <apriyanriyan23@gmail.com>
2022-03-12 16:42:16 +0700

    Push Project

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:online_course/home_page.dart';
+
+void main() => runApp(Myapp());
+
+class Myapp extends StatefulWidget {
+  const Myapp({ Key? key }) : super(key: key);
+
+  @override
+  State<Myapp> createState() => _MyappState();
+}
+
+class _MyappState extends State<Myapp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: home_page(),
+    );
+  }
+}
